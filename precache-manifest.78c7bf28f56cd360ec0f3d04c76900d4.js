88ebf02c694d55bd032d96127080101d64cd1a93
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-29 16:54:29 +0300

    Updates

---
+++ precache-manifest.78c7bf28f56cd360ec0f3d04c76900d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc77a4acbe1e477f8393c720a64be5f2",
+    "url": "/react_carousel/index.html"
+  },
+  {
+    "revision": "9e566a25158de3b7ce33",
+    "url": "/react_carousel/static/css/main.de8cc24d.chunk.css"
+  },
+  {
+    "revision": "f730d0c4bbf0eeb965aa",
+    "url": "/react_carousel/static/js/2.a717a1b1.chunk.js"
+  },
+  {
+    "revision": "9e566a25158de3b7ce33",
+    "url": "/react_carousel/static/js/main.633159e3.chunk.js"
+  },
+  {
+    "revision": "a62ff4406f9253df9292",
+    "url": "/react_carousel/static/js/runtime~main.30902e3a.js"
+  }
+]);
